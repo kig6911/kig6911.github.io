4e77a97f45df5b59a44bfae323beae603250bbe4
kig6911 <brittani.kiger@gmail.com>
2018-03-17 14:03:12 -0400

    Website started

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="styleSheet.css" />
+    
+
+</head>
+
+<body>
+
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="portfolio.php">Portfolio</a></li>
+        <li><a href="projects.php">Projects</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+
+    
+    <br /> <br /> <br /> <br /> <br /> 
+    <img class="img-circle" src="profphoto.jpg" height="200" width="200" position: center;>
+    
+
+
+</body>
+</html>
